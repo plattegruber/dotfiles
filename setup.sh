b8040b821e54ca306d312437052e7913848b3aee
plattegruber <gruberplatte@gmail.com>
2024-04-03 21:19:20 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,4 +3,7 @@
 DOTFILES=$HOME/dotfiles
 
 # ln -sf $DOTFILES/nvim/init.vim $HOME/.config/nvim/init.vim
+
+# brew install tmux
+# git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 # ln -sf $DOTFILES/tmux/.tmux.conf $HOME/.tmux.conf
